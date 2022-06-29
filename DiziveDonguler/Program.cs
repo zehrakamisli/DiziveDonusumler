1d42325a257314ff00c6802c4348eb021a9eead0
Zehra Kamisli <kamsl.zehra97@gmail.com>
2022-06-29 16:29:53 +0300

    New Project

---
+++ DiziveDonguler/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiziveDonguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi Tanımlama
+            string[] Renkler = new string[5];
+            string[] Hayvanlar = {"Kuş","Ayı","Kaplumbağa","Deve","Aslan"};
+
+            int[] Dizi;
+            Dizi=new int[3];
+
+            //Dizilere değer Atama ve Erişim
+            Renkler[0] = "Mavi";
+            Dizi[2] = 10;
+            Console.WriteLine(Hayvanlar[2]);
+            Console.WriteLine(Dizi[2
+                ]);
+            Console.WriteLine(Renkler[0]);
+
+            //Döngülerle Dizi kullanımı
+            //Klavyeden girilen n tane sayının ortalmasını hesaplayan program.
+            Console.Write("Lütfen dizinin eleman sayısını giriniz: ");
+            int DiziUzunlugu=int.Parse(Console.ReadLine());
+            int[] SayıDizisi=new int[DiziUzunlugu];
+
+            for (int i = 0; i < DiziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayısını giriniz.", i+1);
+                SayıDizisi[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            int Toplam = 0;
+            foreach(var sayi in SayıDizisi)
+            {
+                Toplam += sayi;
+                Console.WriteLine(Toplam);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Ortalama: " +Toplam/DiziUzunlugu);
+            Console.Read();
+
+        }
+    }
+}
